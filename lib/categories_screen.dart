933cec850d5b4c25ca4d654737411e847ae177da
Omer Siddique <mr.omersiddique@gmail.com>
2020-07-13 20:19:36 +1000

    Adding Styling

--- lib/categories_screen.dart
+++ lib/categories_screen.dart
@@ -9,6 +9,7 @@ class CategoriesScreen extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(title: Text('Restaurant')),
       body: GridView(
+        padding: const EdgeInsets.all(25),
         children: DUMMY_CATEGORIES.map( (catData) => CategoryItem(catData.title, catData.color) ).toList(), 
         gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
           maxCrossAxisExtent: 200,
--- lib/main.dart
+++ lib/main.dart
@@ -22,7 +22,15 @@ class MyApp extends StatelessWidget {
         // or simply save your changes to "hot reload" in a Flutter IDE).
         // Notice that the counter didn't reset back to zero; the application
         // is not restarted.
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.pink,
+        accentColor: Colors.amber,
+        canvasColor: Color.fromRGBO(255, 254, 229, 1),
+        fontFamily: 'Raleway',
+        textTheme: ThemeData.light().textTheme.copyWith(
+          bodyText1: TextStyle(color: Color.fromRGBO(20, 51, 51, 1)),          
+          bodyText2: TextStyle(color: Color.fromRGBO(20, 51, 51, 1)),
+          headline5: TextStyle(fontSize: 16, fontFamily: 'RobotoCondensed', fontWeight: FontWeight.bold),
+          ),
         // This makes the visual density adapt to the platform that you run
         // the app on. For desktop platforms, the controls will be smaller and
         // closer together (more dense) than on mobile platforms.
